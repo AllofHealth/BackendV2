da186b600bc65c8639be749131e5241ff0722d8b
3illBaby <thrillanon@outlook.com>
2024-05-05 22:25:17 +0100

    chore(admin.module.ts):added the admin guard as a provider

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,13 +3,16 @@ import { AdminService } from './services/admin.service';
 import { AdminController } from './controllers/admin.controller';
 import { AdminSchema, Admin } from './schema/admin.schema';
 import { AdminDao } from './dao/admin.dao';
+import { AdminGuard } from './guards/admin.guard';
 import { MongooseModule } from '@nestjs/mongoose';
+import { HospitalModule } from 'src/hospital/hospital.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
+    HospitalModule,
   ],
-  providers: [AdminService, AdminDao],
+  providers: [AdminService, AdminDao, AdminGuard],
   controllers: [AdminController],
   exports: [AdminService],
 })
