8c77d9a5f715495cb65a0d370d6aa1d5d70689b4
3illBaby <thrillanon@outlook.com>
2024-05-07 09:27:47 +0100

    refactor(hospital.dao.ts):refactored to be minimal

--- src/hospital/dao/hospital.dao.ts
+++ src/hospital/dao/hospital.dao.ts
@@ -1,13 +1,9 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Hospital } from '../schema/hospital.schema';
 import { Model, Types } from 'mongoose';
-import {
-  CreateHospitalType,
-  HospitalType,
-  PreviewType,
-} from '../interface/hospital.interface';
+import { CreateHospitalType } from '../interface/hospital.interface';
 import { HOSPITAL_PLACEHOLDER } from 'src/shared/constants';
-import { ApprovalStatus, Category, HospitalError } from 'src/shared';
+import { ApprovalStatus, Category } from 'src/shared';
 import { encrypt } from 'src/shared/utils/encrypt.utils';
 
 export class HospitalDao {
@@ -74,107 +70,5 @@ export class HospitalDao {
       { id: { $in: hospitalIds } },
       { $pull: { doctors: { walletAddress: walletAddress } } },
     );
-  }
-  async returnDoctorFromHospital(
-    hospital: HospitalType,
-    walletAddress: string,
-  ): Promise<PreviewType | undefined> {
-    try {
-      const Doctor = hospital.doctors.find((d: PreviewType) => {
-        return d.walletAddress === walletAddress;
-      });
-
-      if (!Doctor) {
-        console.info('Doctor not found');
-      }
-
-      return Doctor;
-    } catch (error) {
-      console.error(error);
-      throw new HospitalError('Error finding doctor');
-    }
-  }
-
-  async returnPharmacistFromHospital(
-    hospital: HospitalType,
-    walletAddress: string,
-  ): Promise<PreviewType | undefined> {
-    try {
-      const pharmacists = hospital.pharmacists.find((d: PreviewType) => {
-        return d.walletAddress === walletAddress;
-      });
-      if (!pharmacists) {
-        console.info('pharmacist not found');
-      }
-
-      return pharmacists;
-    } catch (error) {
-      console.error(error);
-      throw new HospitalError('pharmacists not found');
-    }
-  }
-
-  async removeDoctorFromHospital(
-    hospital: HospitalType,
-    doctorAddress: string,
-  ) {
-    try {
-      hospital.doctors = hospital.doctors.filter(
-        (d: PreviewType) => d.walletAddress !== doctorAddress,
-      );
-      console.info('doctor removed');
-    } catch (error) {
-      console.error(error);
-      throw new HospitalError('Error removing doctor');
-    }
-  }
-
-  async removePharmacistFromHospital(
-    hospital: HospitalType,
-    pharmacistAddress: string,
-  ) {
-    try {
-      hospital.pharmacists = hospital.pharmacists.filter(
-        (d: PreviewType) => d.walletAddress !== pharmacistAddress,
-      );
-      console.info('pharmacist removed');
-    } catch (error) {
-      console.error(error);
-      throw new HospitalError('Error removing pharmacist');
-    }
-  }
-
-  async validateHospitalAdmin(hospital: HospitalType, adminAddress: string) {
-    if (!hospital || !adminAddress || adminAddress.length !== 42) {
-      throw new HospitalError('Error validating parameters');
-    }
-
-    let isAdmin = false;
-    try {
-      if (hospital.admin === adminAddress) {
-        isAdmin = true;
-      }
-      return isAdmin;
-    } catch (error) {
-      console.error(error);
-      throw new HospitalError('Error validating admin');
-    }
-  }
-
-  async validateHospitalExists(hospitalId: Types.ObjectId) {
-    if (!hospitalId) {
-      throw new HospitalError('Invalid or missing hospital id');
-    }
-    try {
-      let hospitalExist = false;
-      const hospital = await this.hospitalModel.findById(hospitalId);
-      if (hospital) {
-        hospitalExist = true;
-      }
-      return hospitalExist;
-    } catch (error) {
-      console.error(error);
-      throw new HospitalError('Error validating hospital');
-    }
   }
 }
