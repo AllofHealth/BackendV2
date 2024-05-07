0c32195b3efc32b2e2963fc4f4bcf4e52234e907
3illBaby <thrillanon@outlook.com>
2024-05-07 13:54:21 +0100

    chore(patient.dto.ts):implemented dto for patients

---
+++ src/patient/dto/patient.dto.ts
@@ -0,0 +1,75 @@
+import {
+  IsEthereumAddress,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreatePatientDto {
+  @IsNumber()
+  id: number;
+
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  age: number;
+
+  @IsOptional()
+  @IsString()
+  profilePicture?: string;
+
+  @IsString()
+  address: string;
+
+  @IsString()
+  city: string;
+
+  @IsString()
+  @IsEthereumAddress()
+  walletAddress: string;
+
+  @IsString()
+  bloodGroup: string;
+
+  @IsString()
+  genotype: string;
+
+  @IsOptional()
+  @IsString()
+  category?: string;
+}
+
+export class UpdatePatientProfileDto {
+  @IsString()
+  @IsEthereumAddress()
+  walletAddress: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  age?: string;
+
+  @IsOptional()
+  @IsString()
+  profilePicture?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  bloodGroup?: string;
+
+  @IsOptional()
+  @IsString()
+  genotype?: string;
+}
