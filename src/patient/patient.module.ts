3d45882a2f46117d03ea06f0b5ad57317143c7f0
3illBaby <thrillanon@outlook.com>
2024-05-27 16:34:04 +0100

    chore(patient.module.ts):added family member schema

--- src/patient/patient.module.ts
+++ src/patient/patient.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PatientService } from './services/patient.service';
 import { PatientController } from './controllers/patient.controller';
-import { Patient, PatientSchema } from './schemas/patient.schema';
+import {
+  FamilyMember,
+  FamilyMemberSchema,
+  Patient,
+  PatientSchema,
+} from './schemas/patient.schema';
 import { PatientDao } from './dao/patient.dao';
 import { PatientGuard } from './guards/patient.guard';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -9,6 +14,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Patient.name, schema: PatientSchema }]),
+    MongooseModule.forFeature([
+      {
+        name: FamilyMember.name,
+        schema: FamilyMemberSchema,
+      },
+    ]),
   ],
   providers: [PatientService, PatientDao, PatientGuard],
   controllers: [PatientController],
