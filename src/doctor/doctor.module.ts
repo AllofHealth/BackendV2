953df9a2c43553823017e613c348ae08db086be5
3illBaby <thrillanon@outlook.com>
2024-06-04 00:59:05 +0100

    refactor(doctor.module.ts):removed approval schema from doctor module

--- src/doctor/doctor.module.ts
+++ src/doctor/doctor.module.ts
@@ -3,12 +3,7 @@ import { DoctorService } from './services/doctor.service';
 import { DoctorController } from './controllers/doctor.controller';
 import { DoctorDao } from './dao/doctor.dao';
 import { MongooseModule } from '@nestjs/mongoose';
-import {
-  Approval,
-  ApprovalSchema,
-  Doctor,
-  DoctorSchema,
-} from './schema/doctor.schema';
+import { Doctor, DoctorSchema } from './schema/doctor.schema';
 import { DoctorGuard } from './guards/doctor.guard';
 import { HospitalModule } from 'src/hospital/hospital.module';
 import { PatientModule } from 'src/patient/patient.module';
@@ -16,9 +11,6 @@ import { PatientModule } from 'src/patient/patient.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Doctor.name, schema: DoctorSchema }]),
-    MongooseModule.forFeature([
-      { name: Approval.name, schema: ApprovalSchema },
-    ]),
     forwardRef(() => HospitalModule),
     forwardRef(() => PatientModule),
   ],
