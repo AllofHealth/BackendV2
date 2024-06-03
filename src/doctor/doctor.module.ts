803f95992cba1c4baab91c867989953e777ff525
3illBaby <thrillanon@outlook.com>
2024-06-03 23:09:11 +0100

    chore(doctor.module.ts:imported approval schema)

--- src/doctor/doctor.module.ts
+++ src/doctor/doctor.module.ts
@@ -3,7 +3,12 @@ import { DoctorService } from './services/doctor.service';
 import { DoctorController } from './controllers/doctor.controller';
 import { DoctorDao } from './dao/doctor.dao';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Doctor, DoctorSchema } from './schema/doctor.schema';
+import {
+  Approval,
+  ApprovalSchema,
+  Doctor,
+  DoctorSchema,
+} from './schema/doctor.schema';
 import { DoctorGuard } from './guards/doctor.guard';
 import { HospitalModule } from 'src/hospital/hospital.module';
 import { PatientModule } from 'src/patient/patient.module';
@@ -11,6 +16,9 @@ import { PatientModule } from 'src/patient/patient.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Doctor.name, schema: DoctorSchema }]),
+    MongooseModule.forFeature([
+      { name: Approval.name, schema: ApprovalSchema },
+    ]),
     forwardRef(() => HospitalModule),
     forwardRef(() => PatientModule),
   ],
