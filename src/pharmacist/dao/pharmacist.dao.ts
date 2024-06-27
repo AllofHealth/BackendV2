f3fc1eaf8140b590bbb7e82ea25839de2b573421
3illBaby <thrillanon@outlook.com>
2024-06-27 23:05:39 +0100

    chore(pharmacist.dao.ts):added dao function to create Inventory, create medicine and update medicine

--- src/pharmacist/dao/pharmacist.dao.ts
+++ src/pharmacist/dao/pharmacist.dao.ts
@@ -1,8 +1,11 @@
 import { InjectModel } from '@nestjs/mongoose';
-import { Pharmacist } from '../schema/pharmacist.schema';
-import { Model } from 'mongoose';
+import { Inventory, Medicine, Pharmacist } from '../schema/pharmacist.schema';
+import { Model, Types } from 'mongoose';
 import {
   CreatePharmacistType,
+  InventoryType,
+  MedicineType,
+  UpdateMedicineType,
   UpdatePharmacistType,
 } from '../interface/pharmacist.interface';
 import { PROFILE_PLACEHOLDER } from 'src/shared/constants';
@@ -12,6 +15,8 @@ export class PharmacistDao {
   constructor(
     @InjectModel(Pharmacist.name)
     private readonly pharmacistModel: Model<Pharmacist>,
+    private readonly medicineModel: Model<Medicine>,
+    private readonly inventoryModel: Model<Inventory>,
   ) {}
   async createNewPharmacist(pharmacist: CreatePharmacistType) {
     return await this.pharmacistModel.create({
@@ -25,6 +30,27 @@ export class PharmacistDao {
       phoneNumber: pharmacist.phoneNumber,
       walletAddress: pharmacist.walletAddress,
       status: ApprovalStatus.Pending,
+    });
+  }
+
+  async createMedicine(args: MedicineType) {
+    return await this.medicineModel.create({
+      name: args.name,
+      price: args.price,
+      quantity: args.quantity,
+      description: args.description,
+      sideEffects: args.sideEffects ? args.sideEffects : 'none',
+      image: args.image ? args.image : 'https://pixy.org/src/144/1448676.jpg',
+      medicineGroup: args.medicineGroup,
+    });
+  }
+
+  async createInventory(args: InventoryType) {
+    return await this.inventoryModel.create({
+      numberOfMedicine: args.numberOfMedicine,
+      numberOfMedicineGroup: args.numberOfMedicineGroup,
+      numberOfMedicineSold: args.numberOfMedicineSold,
+      medicines: args.medicines,
     });
   }
 
@@ -62,8 +88,31 @@ export class PharmacistDao {
       { new: true, runValidators: true },
     );
 
-    console.log('Update result:', result);
     return result;
+  }
+
+  async updateMedicine(
+    medicineId: Types.ObjectId,
+    updateData: UpdateMedicineType,
+  ) {
+    const updates = Object.keys(updateData).reduce((acc, key) => {
+      if (updateData[key] !== undefined) {
+        acc[key] = updateData[key];
+      }
+      return acc;
+    }, {});
+
+    const result = await this.medicineModel.findOneAndUpdate(
+      { _id: medicineId },
+      { $set: updates },
+      { new: true, runValidators: true },
+    );
+
+    return result;
+  }
+
+  async deleteMedicine(medicineId: Types.ObjectId) {
+    return await this.medicineModel.deleteOne({ _id: medicineId });
   }
 
   async deletePharmacist(address: string) {
