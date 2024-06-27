c3b41efa1ef429485e17a792722b27828d81adad
3illBaby <thrillanon@outlook.com>
2024-06-27 21:50:38 +0100

    chore(main.ts):updated port

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.useLogger(app.get(MyLoggerService));
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
