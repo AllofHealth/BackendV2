e8435014939480cfdbc629954e1120ab26ad306e
3illBaby <thrillanon@outlook.com>
2024-06-28 20:17:11 +0100

    chore(pharmacists.service.ts):added new medicine placeholder

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -8,3 +8,4 @@ export const PROFILE_PLACEHOLDER =
   'https://www.kindpng.com/picc/m/722-7221920_placeholder-profile-image-placeholder-png-transparent-png.png';
 export const HOSPITAL_PLACEHOLDER =
   'https://www.kindpng.com/picc/m/264-2646273_hydrodent-micro-channel-icon-hospital-navigation-icon-hospital.png';
+export const MEDICINE_PLACEHOLDER = 'https://pixy.org/src/144/1448676.jpg';
