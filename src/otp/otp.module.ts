1ffb289e819ac28ac3b4347db54ecbbc780d0588
3illBaby <thrillanon@outlook.com>
2024-07-24 23:57:25 +0100

    chore(otp.module.ts):added controller to otp module

--- src/otp/otp.module.ts
+++ src/otp/otp.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { OtpService } from './otp.service';
+import { OtpService } from './services/otp.service';
+import { OtpController } from './controller/otp/otp.controller';
 
 @Module({
-  providers: [OtpService]
+  providers: [OtpService],
+  controllers: [OtpController],
 })
 export class OtpModule {}
