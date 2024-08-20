e253c36e00f1d6bbfe5029dd867c583c9bc7b625
3illBaby <thrillanon@outlook.com>
2024-08-20 19:14:10 +0100

    refactor(otp.interface.ts): refactored interface

--- src/modules/otp/interface/otp.interface.ts
+++ src/modules/otp/interface/otp.interface.ts
@@ -1,4 +1,12 @@
+export type RoleType =
+  | 'doctor'
+  | 'pharmacist'
+  | 'institution'
+  | 'admin'
+  | 'patient';
+
 export interface OtpEntry {
   otp: string;
   expiresAt: number;
+  role: RoleType;
 }
