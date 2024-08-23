cbf2f8f8b848822c708f8a5373d4f6d12feebddc
3illBaby <thrillanon@outlook.com>
2024-08-23 21:35:32 +0100

    refactor(medicine.schema.ts):init receipt schema before medicine schema

--- src/modules/medicine/schema/medicine.schema.ts
+++ src/modules/medicine/schema/medicine.schema.ts
@@ -2,6 +2,23 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @Schema()
+export class Receipt extends Document {
+  @Prop({ required: true })
+  productDispensed: string;
+
+  @Prop({ required: true, default: new Date(Date.now()) })
+  dateDispensed: Date;
+
+  @Prop({ required: true })
+  directions: string;
+
+  @Prop({ required: true })
+  quantity: string;
+}
+
+export const ReceiptSchema = SchemaFactory.createForClass(Receipt);
+
+@Schema()
 export class Medicine extends Document {
   @Prop({ required: true })
   productPrescribed: string;
@@ -23,17 +40,3 @@ export class Medicine extends Document {
 }
 
 export const MedicineSchema = SchemaFactory.createForClass(Medicine);
-
-export class Receipt extends Document {
-  @Prop({ required: true })
-  productDispensed: string;
-
-  @Prop({ required: true, default: new Date(Date.now()) })
-  dateDispensed: Date;
-
-  @Prop({ required: true })
-  directions: string;
-
-  @Prop({ required: true })
-  quantity: string;
-}
