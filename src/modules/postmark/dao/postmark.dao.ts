d838a356df4fc9d15d0acb5be0ef0aa6c966ce3d
3illBaby <thrillanon@outlook.com>
2024-08-24 23:47:34 +0100

    refactor(postmark.dao.ts):loaded env from auth config

--- src/modules/postmark/dao/postmark.dao.ts
+++ src/modules/postmark/dao/postmark.dao.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { ServerClient } from 'postmark';
 import { AuthConfiguration } from '@/shared/config/auth.configuration';
-import { POSTMARK_SERVER_TOKEN } from '@/shared/constants';
 
 @Injectable()
 export class PostmarkDao {
   constructor(private readonly config: AuthConfiguration) {}
   provideClient() {
-    const client = new ServerClient(POSTMARK_SERVER_TOKEN);
+    const client = new ServerClient(this.config.SERVER_TOKEN);
     return client;
   }
 
