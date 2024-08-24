447d04789192d45bcbec61ee24a4b4ebf4c770c8
3illBaby <thrillanon@outlook.com>
2024-08-24 21:54:26 +0100

    chore(admin.auth.guard.ts):init and implemented auth guard

---
+++ src/modules/admin/guards/admin.auth.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { AdminService } from '../services/admin.service';
+
+@Injectable()
+export class AdminAuthGuard implements CanActivate {
+  constructor(private readonly adminService: AdminService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const adminAddress = request.query.adminAddress;
+
+    if (!adminAddress) {
+      throw new ForbiddenException('please connect wallet');
+    }
+
+    if (!(await this.adminService.isAdminAuthenticated(adminAddress))) {
+      throw new ForbiddenException('Not an authenticated admin');
+    }
+
+    return true;
+  }
+}
