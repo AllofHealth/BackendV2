60b9bac721cce19028402e48ed3886a5797a880e
3illBaby <thrillanon@outlook.com>
2024-08-24 23:48:21 +0100

    refactor(auth.configuration.ts):added class validators

--- src/shared/config/auth.configuration.ts
+++ src/shared/config/auth.configuration.ts
@@ -1,10 +1,20 @@
 import { Configuration, Value } from '@itgorillaz/configify';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 @Configuration()
 export class AuthConfiguration {
+  @IsNotEmpty()
+  @IsString()
   @Value('MONGODB_URI')
   MONGO_URI: string;
 
+  @IsNotEmpty()
+  @IsString()
   @Value('POSTMARK_SERVER_TOKEN')
   SERVER_TOKEN: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Value('ENCRYPTION_KEY')
+  ENCRYPTION_KEY: string;
 }
