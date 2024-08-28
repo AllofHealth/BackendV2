c06746e7a78d7601dda6273668000d1c97ae197a
3illBaby <thrillanon@outlook.com>
2024-08-28 11:40:00 +0100

    chore(patient.auth.guard.ts):impl verification guard

---
+++ src/modules/patient/guards/patient.auth.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PatientDao } from '../dao/patient.dao';
+
+@Injectable()
+export class PatientAuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const patientAddress = request.query.walletAddress;
+
+    if (!patientAddress) {
+      throw new UnauthorizedException('Please connect your wallet');
+    }
+    return true;
+  }
+}
+
+export class PatientVerificationGuard implements CanActivate {
+  constructor(private readonly patientDao: PatientDao) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const patientAddress = request.query.walletAddress;
+    const patient = await this.patientDao.fetchPatientByAddress(patientAddress);
+
+    if (!patient && !patient.isVerified) {
+      throw new ForbiddenException('please complete verification');
+    }
+    return true;
+  }
+}
