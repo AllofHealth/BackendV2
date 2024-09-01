35a3ccc8a33e79962e5ddc971e437b9f912b982e
3illBaby <thrillanon@outlook.com>
2024-09-01 18:18:14 +0100

    chore(pharmacist.interface.ts):added new interface to fetch and delete medication

--- src/modules/pharmacist/interface/pharmacist.interface.ts
+++ src/modules/pharmacist/interface/pharmacist.interface.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { Types } from 'mongoose';
 
 export interface MedicineType {
   name: string;
@@ -14,6 +14,14 @@ export interface ProductType {
   description?: string;
   medications: MedicineType[];
 }
+
+export interface DeleteMedicineInterface {
+  walletAddress: string;
+  productId: Types.ObjectId;
+  medicineId: Types.ObjectId;
+}
+
+export interface FetchMedicineInterface extends DeleteMedicineInterface {}
 
 export interface UpdateMedicineType {
   name?: string;
