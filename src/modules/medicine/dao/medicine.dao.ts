0db2fc3181c11f6a6b0c08e270c0f49b1c52ca93
3illBaby <thrillanon@outlook.com>
2024-09-03 16:44:01 +0100

    chore(medicine.dao.ts):impl func to create receipt

--- src/modules/medicine/dao/medicine.dao.ts
+++ src/modules/medicine/dao/medicine.dao.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Medication, MedicineCategories } from '../schema/medicine.schema';
+import {
+  Medication,
+  MedicineCategories,
+  Receipt,
+} from '../schema/medicine.schema';
 import { Model } from 'mongoose';
-import { CreateMedicineInterface } from '../interface/medicine.interface';
+import {
+  CreateMedicineInterface,
+  CreateReceiptInterface,
+} from '../interface/medicine.interface';
 import { drugClasses } from '@/shared/constants';
 
 @Injectable()
@@ -11,10 +18,21 @@ export class MedicineDao {
     @InjectModel(Medication.name) private readonly medicine: Model<Medication>,
     @InjectModel(MedicineCategories.name)
     private readonly categories: Model<MedicineCategories>,
+    @InjectModel(Receipt.name) private readonly receipt: Model<Receipt>,
   ) {}
 
   async createMedicine(args: CreateMedicineInterface) {
     return await this.medicine.create(args);
+  }
+
+  async createReceipt(args: CreateReceiptInterface) {
+    return await this.receipt.create({
+      productDispensed: args.productDispensed,
+      dateDispensed: new Date(Date.now()),
+      directions: args.directions,
+      quantity: args.quantity,
+      price: args.price,
+    });
   }
 
   async getAllCategories() {
