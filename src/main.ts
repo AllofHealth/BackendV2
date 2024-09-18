a2189022b6e03f1cc2b931e8e19872dbed3ff57d
3illBaby <thrillanon@outlook.com>
2024-09-18 13:50:31 +0100

    Remove unused import from main.ts
    The MyLoggerService import in main.ts was not being used and has been removed to clean up the code. This change reduces possible confusion and improves maintainability.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MyLoggerService } from './modules/my-logger/my-logger.service';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
