720f5928eb3ac698d879fa92e6d090ee337af065
3illBaby <thrillanon@outlook.com>
2024-09-18 13:51:27 +0100

    Refactor and fix logging service
    Replaced synchronous path verification with asynchronous fsPromises for directory creation and file writing. Updated the time zone to 'Africa/Lagos' for time formatting. Improved error handling for file write operations and removed unnecessary `await` keywords.

--- src/modules/my-logger/my-logger.service.ts
+++ src/modules/my-logger/my-logger.service.ts
@@ -1,46 +1,48 @@
 import { ConsoleLogger, Injectable } from '@nestjs/common';
 import * as fs from 'fs';
-import { promises as fsPromises } from 'fs';
+import * as fsPromises from 'fs/promises';
 import * as path from 'path';
 
 @Injectable()
 export class MyLoggerService extends ConsoleLogger {
   async logToFile(entry: string) {
     const formattedEntry = `${Intl.DateTimeFormat('en-US', {
-      timeZone: 'WAT',
+      timeZone: 'Africa/Lagos',
       dateStyle: 'short',
       timeStyle: 'short',
     }).format(new Date())}\t${entry}\n`;
-
     try {
-      const pathExist = fs.existsSync(path.join(__dirname, '..', '..', 'logs'));
+      // Verifying and creating the logs directory
+      const logsDir = path.resolve(__dirname, '..', '..', 'logs');
+      const pathExist = fs.existsSync(logsDir);
+
       if (!pathExist) {
-        await fsPromises.mkdir(path.join(__dirname, '..', '..', 'logs'));
+        await fsPromises.mkdir(logsDir, { recursive: true });
       }
-      await fsPromises.appendFile(
-        path.join(__dirname, '..', '..', 'logs', 'myLogFile.log'),
-        formattedEntry,
-      );
+
+      // Writing to the log file
+      const logFilePath = path.join(logsDir, 'myLogFile.log');
+      await fsPromises.appendFile(logFilePath, formattedEntry);
     } catch (e) {
-      if (e instanceof Error) console.error(e);
+      console.error('Error writing to the log file:', e);
     }
   }
 
   log(message: any, context?: string) {
     const entry = `${context}\t${message}`;
-    await this.logToFile(entry);
+    this.logToFile(entry);
     super.log(message, context);
   }
 
   error(message: any, stackOrContext?: string) {
     const entry = `${stackOrContext}\t${message}`;
-    await this.logToFile(entry);
+    this.logToFile(entry);
     super.error(message, stackOrContext);
   }
 
   info(message: any, context?: string) {
     const entry = `${context}\t${message}`;
-    await this.logToFile(entry);
+    this.logToFile(entry);
     super.verbose(message, context);
   }
 }
