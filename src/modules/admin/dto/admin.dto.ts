a3d7306119358637338c890818f8921c6c3579e3
3illBaby <thrillanon@outlook.com>
2024-09-24 16:15:37 +0100

    refactor(admin.dto.ts): api property
    included api property for a more comprehensive documentation

--- src/modules/admin/dto/admin.dto.ts
+++ src/modules/admin/dto/admin.dto.ts
@@ -5,13 +5,23 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
+  IsUrl,
 } from 'class-validator';
 import { Types } from 'mongoose';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class ApproveHospitalDto {
+  @ApiProperty({
+    description: 'hospital mongo uuid',
+    type: Types.ObjectId,
+  })
   @IsMongoId()
   hospitalId: Types.ObjectId;
 
+  @ApiProperty({
+    description: 'admin ethereum address',
+    type: String,
+  })
   @IsString()
   @IsEthereumAddress()
   @IsNotEmpty()
@@ -19,30 +29,54 @@ export class ApproveHospitalDto {
 }
 
 export class CreateAdminDto {
+  @ApiProperty({
+    description: 'admin name',
+    type: String,
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
-  @IsString()
+  @ApiPropertyOptional({
+    description: 'profile picture url',
+    type: String,
+  })
+  @IsUrl()
   @IsOptional()
   profilePicture?: string;
 
+  @ApiProperty({
+    description: 'email address',
+    type: String,
+  })
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'admin ethereum address',
+    type: String,
+  })
   @IsEthereumAddress()
   @IsNotEmpty()
   walletAddress: string;
 }
 
 export class RemoveAdminDto {
+  @ApiProperty({
+    description: 'admin ethereum address to authorize',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   @IsEthereumAddress()
   adminAddressToAuthorize: string;
 
+  @ApiProperty({
+    description: 'admin ethereum address to remove',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   @IsEthereumAddress()
@@ -50,16 +84,27 @@ export class RemoveAdminDto {
 }
 
 export class UpdateAdminDto {
-  @IsString()
+  @ApiPropertyOptional({
+    description: 'admin name',
+    type: String,
+  })
   @IsOptional()
+  @IsString()
   name?: string;
 
-  @IsString()
+  @ApiPropertyOptional({
+    description: 'profile picture url',
+    type: String,
+  })
   @IsOptional()
+  @IsUrl()
   profilePicture?: string;
 
-  @IsString()
-  @IsEmail()
+  @ApiPropertyOptional({
+    description: 'admin email',
+    type: String,
+  })
   @IsOptional()
+  @IsEmail()
   email?: string;
 }
