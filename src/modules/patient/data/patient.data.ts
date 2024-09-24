ec6bea0c3d0d010f3a6e75d7cf072e767906361a
3illBaby <thrillanon@outlook.com>
2024-09-24 23:54:18 +0100

    chore(patient.data.ts): added new error and success messages

--- src/modules/patient/data/patient.data.ts
+++ src/modules/patient/data/patient.data.ts
@@ -3,6 +3,7 @@ export enum PatientErrors {
   PATIENT_CREATED_ERROR = 'an error occurred while creating patient',
   PATIENT_FETCH_ERROR = 'an error occurred while fetching patients',
   PATIENT_NOT_FOUND = 'patient not found',
+  PATIENT_UPDATE_ERROR = 'an error occurred while updating patient',
   FAMILY_MEMBER_NOT_FOUND = 'family member not found',
   FAMILY_MEMBER_EXIST = 'family member already exists',
   FAMILY_MEMBER_ERROR = 'an error occurred while adding family member',
@@ -13,6 +14,7 @@ export enum PatientErrors {
 
 export enum PatientSuccess {
   PATIENT_CREATED = 'patient created successfully',
+  PATIENT_UPDATED = 'patient updated successfully',
   FAMILY_MEMBER_ADDED = 'family member added successfully',
   FAMILY_MEMBER_FOUND = 'family member found',
   FAMILY_MEMBER_UPDATED = 'family member updated successfully',
