0942f1e649aabdec4d6953e8023f1ced963a82cb
3illBaby <thrillanon@outlook.com>
2024-09-28 08:34:43 +0100

    refactor(medicine.dto.ts): removed props

--- src/modules/medicine/dto/medicine.dto.ts
+++ src/modules/medicine/dto/medicine.dto.ts
@@ -1,35 +1,27 @@
-import { Prop } from '@nestjs/mongoose';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Types } from 'mongoose';
 import { ReceiptDto } from '@/modules/patient/dto/patient.dto';
 
 export class MedicineDto {
   @ApiProperty({ type: String })
-  @Prop({ required: true })
   productPrescribed: string;
 
   @ApiProperty({ type: String })
-  @Prop({ required: true })
   productCategory: string;
 
   @ApiProperty({ type: String })
-  @Prop({ required: true })
   productDosage: string;
 
   @ApiPropertyOptional({ type: String })
-  @Prop()
   practitionerNote: string;
 
   @ApiProperty({ type: Date })
-  @Prop({ default: new Date(Date.now()) })
   date: Date;
 
   @ApiProperty({ type: Boolean, default: false })
-  @Prop({ default: false })
   isDispensed: boolean;
 
   @ApiPropertyOptional({ type: ReceiptDto })
-  @Prop()
   receipt: ReceiptDto;
 
   @ApiProperty({ type: Types.ObjectId })
